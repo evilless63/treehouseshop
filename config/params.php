3e8e43a8e9ff32ed2b755523b0627bd15685aee5
macos <macos@MacBook-Pro-macos.local>
2021-01-27 12:36:14 -0800

    save

--- config/params.php
+++ config/params.php
@@ -2,15 +2,15 @@
 
     return [
         'admin_email' => 'admin@admin.com',
-        'shop_name' => 'Laravel Shop',
+        'shop_name' => 'Дом на дереве',
         'pagination' => 3,
         'smtp_host' => 'smtp.com.net',
         'smtp_port' => '2525',
         'smtp_protocol' => 'ssl',
         'smtp_login' => 'laravel@laravel.com',
         'smtp_password' => '12345678',
-        'img_width' => 125,
-        'img_height' => 200,
+        'img_width' => 700,
+        'img_height' => 1000,
         'gallery_width' => 700,
         'gallery_height' => 1000,
     ];
