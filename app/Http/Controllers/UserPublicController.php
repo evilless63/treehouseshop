72c8a0cbddbed8f2bb68fd65cd00c15d2080b855
macos <macos@MacBook-Pro-macos.local>
2021-01-28 18:34:16 -0800

    instagram

--- app/Http/Controllers/UserPublicController.php
+++ app/Http/Controllers/UserPublicController.php
@@ -34,7 +34,8 @@ class UserPublicController extends UserBaseController
     public function index() {
 
         $instagram = \InstagramScraper\Instagram::withCredentials(new \GuzzleHttp\Client(), 'iraemelianova', 'alena210813', new Psr16Adapter('Files'));
-        $instagram->login();
+        $instagram->setUserAgent('User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0');
+        $instagram->login(false, true);
         $instagram->saveSession();
         $instagram_posts = $instagram->getMedias('dom_na_dereve', 10);
 
